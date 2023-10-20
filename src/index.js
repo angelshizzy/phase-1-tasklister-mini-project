a3522f9eb0f20cbd53c5abd00b1d9584d7be424e
Angela Shirao <angela.shirao@student.moringaschool.com>
2023-10-20 17:32:22 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form= document.querySelector('form').addEventListener("submit", e)=
+  e.preventDefault()
+  console.log(e.target.new_description.value)
+  
+  let taskValue= e.target.todo.value;
+  let task = document.getElementById("tasks")
+  let li = document.createElement("li")
+  letbutton= document.createElement("button")
+
+  li.appendChild(button)
+  console,log(task)
+  task.appendChild(li)
+  form.reset
 });
+
+// add delete function
+function handleDelete(e){
+  e.target.parentNode.remove
+}
